c8f916ab93290cab6405796997c4505f65810ac5
thierry <thierrymoumi@gmail.com>
2017-01-17 21:35:34 +0100

    code wilfried modifié

--- src/helloword/HelloWord.java
+++ src/helloword/HelloWord.java
@@ -3,8 +3,8 @@ package helloword;
 public class HelloWord {
 	
 	public static void main (String[] args){
-		
 		System.out.println("bienvenu sur github!!!!! salut thierry");
+		System.out.println("Merci:)");
 	}
 
 }
